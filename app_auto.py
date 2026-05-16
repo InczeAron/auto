e4d1b3f9208130fe44e836d4a2854474fdb9863e
InczeAron <74004041+InczeAron@users.noreply.github.com>
2026-05-16 21:20:31 +0200

    merci jav

--- app_auto.py
+++ app_auto.py
@@ -337,14 +337,14 @@ def run_scraper():
             "dealer_id": "dealer1",
             "emails": ["aronincze@aronsoft.hu"],
             "searches": [
-                {"brand": "audi", "model": "a6",           "year_from": 2024, "year_to": 2026, "country": "A"},
+                {"brand": "audi", "model": "a6", "year_from": 2024, "year_to": 2026, "country": "A"},
             ]
         },
         {
             "dealer_id": "dealer2",
             "emails": ["inczearon@gmail.com"],
             "searches": [
-                {"brand": "mercedes-benz",        "model": "gla",   "year_from": 2024, "year_to": 2026, "country": "D"},
+                {"brand": "mercedes-benz", "model": "gla-(all)", "year_from": 2024, "year_to": 2026, "country": "D"},
             ]
         },
         {
