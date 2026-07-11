f342796fd56433ee2a337d251e8e1e8afca4d139
InczeAron <74004041+InczeAron@users.noreply.github.com>
2026-07-11 20:55:46 +0200

    update1

--- app_auto.py
+++ app_auto.py
@@ -332,8 +332,6 @@ def scrape_search(page, brand, model_slug, year_from, year_to, country):
                 except:
                     location = ""
 
-                print("LINK:", link)
-
                 cars.append({
                     "Sorszám": len(cars) + 1,
                     "Cím":     title,
@@ -370,21 +368,21 @@ def run_scraper():
             ]
         },
         {
-            "dealer_id": "dealer2",
+            "dealer_id": "dealer1",
             "emails": ["aronincze@aronsoft.hu"],
             "searches": [
                 {"brand": "audi", "model": "a6", "year_from": 2024, "year_to": 2026, "country": "A"},
             ]
         },
         {
-            "dealer_id": "dealer3",
+            "dealer_id": "dealer2",
             "emails": ["inczearon@gmail.com"],
             "searches": [
                 {"brand": "mercedes-benz", "model": "gla-(all)", "year_from": 2024, "year_to": 2026, "country": "D"},
             ]
         },
         {
-            "dealer_id": "dealer4",
+            "dealer_id": "dealer2",
             "emails": ["inczearon@gmail.com"],
             "searches": [
                 {"brand": "volkswagen", "model": "golf", "year_from": 2024, "year_to": 2026, "country": "D"},
